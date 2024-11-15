7abc153943c48b6183015f77cfa3f6b051a07f90
akhumly <164754171+akhumly@users.noreply.github.com>
2024-11-15 15:39:05 +0000

    97 halfway

---
+++ 05-onwards-foodies-starting-project/next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second when running in devcontainer that is blocking regular hot module reload
+        aggregateTimeout: 300, // Delay before rebuilding
+        ignored: ['**/node_modules']
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
